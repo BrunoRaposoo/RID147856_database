4fa5b36fe3cb3742afdfd8408179a5f2b719d567
Bruno Raposo <brunoraposounb@gmail.com>
2025-09-08 15:53:40 -0300

    Feat: add databse mysql

--- index.js
+++ index.js
@@ -4,93 +4,93 @@ const port = 8080;
 
 app.use(express.json());
 
-app.post("/produtos", (req, res) => {
+app.post("/products", (req, res) => {
   res.status(200).json({
     data: "cadastrar produtos"
   });
 });
 
-app.get("/produtos", (req, res) => {
+app.get("/products", (req, res) => {
   res.status(200).json({
     data: 'lista de produtos'
   })
 });
 
-app.get("/produtos/:id", (req, res) => {
+app.get("/products/:id", (req, res) => {
   res.status(200).json({
     data: 'produto específico'
   })
 });
 
-app.put("/produtos/:id", (req, res) => {
+app.put("/products/:id", (req, res) => {
   res.status(200).json({
     data: 'atualizar produto específico'
   })
 });
 
-app.delete("/produtos/:id", (req, res) => {
+app.delete("/products/:id", (req, res) => {
   res.status(200).json({
     data: 'produto deletado',
     message: 'Produto deletado com sucesso'
   })
 });
 
-app.post("/clientes", (req, res) => {
+app.post("/customers", (req, res) => {
   res.status(200).json({
     data: 'Cadastrar clientes'
   })
 });
 
-app.get("/clientes", (req, res) => {
+app.get("/customers", (req, res) => {
   res.status(200).json({
     data: 'lista de clientes'
   })
 });
 
-app.get("/clientes/:id", (req, res) => {
+app.get("/customers/:id", (req, res) => {
   res.status(200).json({
     data: 'cliente específico'
   })
 });
 
-app.put("/clientes/:id", (req, res) => {
+app.put("/customers/:id", (req, res) => {
   res.status(200).json({
     data: 'atualizar cliente'
   })
 });
 
-app.delete("/clientes/:id", (req, res) => {
+app.delete("/customers/:id", (req, res) => {
   res.status(200).json({
     data: 'Deletar cliente',
     message: 'Cliente deletado com sucesso'
   })
 });
 
-app.post("/vendas", (req, res) => {
+app.post("/sales", (req, res) => {
   res.status(200).json({
     data: 'Venda criada'
   })
 });
 
-app.get("/vendas", (req, res) => {
+app.get("/sales", (req, res) => {
   res.status(200).json({
     data: 'vendas'
   })
 });
 
-app.get("/vendas/:id", (req, res) => {
+app.get("/sales/:id", (req, res) => {
   res.status(200).json({
     data: 'venda específica'
   })
 });
 
-app.get("/estoque", (req, res) => {
+app.get("/inventory", (req, res) => {
   res.status(200).json({
     data: 'estoque'
   })
 });
 
-app.put("/estoque/:id_produto", (req, res) => {
+app.put("/inventory/:id_produto", (req, res) => {
   res.status(200).json({
     data: 'estoque de produto específico'
   })
@@ -100,10 +100,10 @@ app.get("/", (req, res) => {
   res.json({
     mensagem: "API DNCommerce funcionando!",
     endpoints: {
-      produtos: "/produtos",
-      clientes: "/clientes",
-      vendas: "/vendas",
-      estoque: "/estoque",
+      produtos: "/products",
+      clientes: "/customers",
+      vendas: "/sales",
+      estoque: "/inventory", 
     },
   });
 });
